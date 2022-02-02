50ba03544dbc5c7313226195fe705a63eb8334f3
git <shorooq.hasan@diconium.com>
2022-02-02 16:21:22 +0100

    delete comments

--- src/main/java/com/client/project/api/model/WeatherModel.java
+++ src/main/java/com/client/project/api/model/WeatherModel.java
@@ -10,19 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class WeatherModel {
-    //private List<String> weather = new ArrayList<>();
 
     private Integer id = null;
     private String main = null;
     private String description = null;
     private String icon = null;
 
-   // ArrayList<String> listOfStrings = new ArrayList<String>(main,description,icon);
-   // ArrayList<Integer> listOfIntegers = new ArrayList<Integer>(id);
-   // List<List> weather = new ArrayList<List>(listOfIntegers,listOfStrings);
-
-
-  //  private List<String> weather =new ArrayList<String>( id,main,description,icon);
 
 
 }
